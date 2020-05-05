76a3e3ea65a1406efc2c67ce9b823761a2cb404b
AlexPoilrouge <alexandre.hurstel@mailoo.org>
2020-05-05 12:03:14 +0200

    adding option to client install script

--- client/install_client.sh
+++ client/install_client.sh
@@ -4,6 +4,8 @@
 SCRIPT_NAME=$( basename "$0" )
 SCRIPT_DIR_PATH=$( realpath "$( dirname "$0" )" )
 
+ROOT=""
+
 USER_INSTALL_DIR="${HOME}/.remote_tvcmd"
 SYSTEM_INSTALL_DIR="/usr/share/remote_tvcmd"
 
@@ -32,7 +34,7 @@ echo_desktop_file(){
 
 
  
-if ! OPTS=$( getopt -o : --long uninstall,system -- "$@" );
+if ! OPTS=$( getopt  -l system,uninstall,root: -o : -- "$@" );
 then
     echo >&2 "OPTS: ${OPTS}"
     echo >&2 "Unexpected error while reading options and commands …"
@@ -49,47 +51,56 @@ while true ; do
     ;;
     --uninstall)
         MODE="uninstall"
-        ;;
+        shift
+    ;;
+    --root)
+        ROOT="$2"
+        shift 2
+    ;;
     --) shift; break;;
     esac
 done
 
-if [[ "${INSTALL_DIR}" == "${SYSTEM_INSTALL_DIR}" ]] && (( EUID != 0 )); then
+if [[ "${ROOT}${INSTALL_DIR}" == "${ROOT}${SYSTEM_INSTALL_DIR}" ]] && (( EUID != 0 )); then
     echo "${SCRIPT_NAME} - you would need root privilege to ${MODE} on system"
     exit 1
 fi
 
-if [ ! -d ${INSTALL_DIR} ] && ! ( mkdir -p "${INSTALL_DIR}" ) ; then
-    echo "${SCRIPT_NAME} - couldn't found nor create directory ${INSTALL_DIR} for ${MODE}…"
+if [ ! -d "${ROOT}${INSTALL_DIR}" ] && ! ( mkdir -p "${ROOT}${INSTALL_DIR}" ) ; then
+    echo "${SCRIPT_NAME} - couldn't found nor create directory ${ROOT}${INSTALL_DIR} for ${MODE}…"
     exit 2
 fi
 
-if [[ "${APP_DIR}" == "${SYSTEM_APP_DIR}" ]] && (( EUID != 0 )); then
+if [[ "${ROOT}${APP_DIR}" == "${ROOT}${SYSTEM_APP_DIR}" ]] && (( EUID != 0 )); then
     echo "${SCRIPT_NAME} - you would need root privilege to ${MODE} on system"
     exit 3
 fi
 
-if [ ! -d ${APP_DIR} ] && ! ( mkdir -p "${APP_DIR}" ) ; then
-    echo "${SCRIPT_NAME} - couldn't found nor create directory ${APP_DIR} for ${MODE}"
+if [ ! -d "${ROOT}${APP_DIR}" ] && ! ( mkdir -p "${ROOT}${APP_DIR}" ) ; then
+    echo "${SCRIPT_NAME} - couldn't found nor create directory ${ROOT}${APP_DIR} for ${MODE}"
     exit 4
 fi
 
 if [[ "${MODE}" == "install" ]]; then
-    mkdir -p "${INSTALL_DIR}"/gui || (echo "error installation: 'mkdir -p ${SCRIPT_DIR_PATH}/gui' failed…"; exit 5)
-    cp -rvf "${SCRIPT_DIR_PATH}"/{client.sh,reciever.sh,sender.sh} "${INSTALL_DIR}"
-    cp -rvf "${SCRIPT_DIR_PATH}"/gui/{GUI.py,gui.glade} "${INSTALL_DIR}"/gui
+    mkdir -p "${ROOT}${INSTALL_DIR}"/gui || (echo "error installation: 'mkdir -p ${ROOT}${INSTALL_DIR}/gui' failed…"; exit 5)
+    cp -rvf "${SCRIPT_DIR_PATH}"/{client.sh,reciever.sh,sender.sh} "${ROOT}${INSTALL_DIR}"
+    cp -rvf "${SCRIPT_DIR_PATH}"/gui/{GUI.py,gui.glade} "${ROOT}${INSTALL_DIR}"/gui
 
-    echo_desktop_file > "${APP_DIR}/remote_tvcmd.desktop"
+    echo_desktop_file > "${ROOT}${APP_DIR}/remote_tvcmd.desktop"
 
-    if [[ "${INSTALL_DIR}" == "${SYSTEM_INSTALL_DIR}" ]]; then
-        ln -s "${_SYSTEM_BIN_LINK}" "${INSTALL_DIR}/client.sh"
+    if [[ "${ROOT}${INSTALL_DIR}" == "${ROOT}${SYSTEM_INSTALL_DIR}" ]]; then
+        if ! ( mkdir -p "$( realpath "$( dirname "${ROOT}${_SYSTEM_BIN_LINK}" )" )" ); then
+            echo "${SCRIPT_NAME} - could not create or access directory containing link '${ROOT}${_SYSTEM_BIN_LINK}'"
+        else
+            ln -s "${ROOT}${INSTALL_DIR}/client.sh" "${ROOT}${_SYSTEM_BIN_LINK}"
+        fi
     fi
 elif [[ "${MODE}" == "uninstall" ]]; then
-    if [[ "${INSTALL_DIR}" == "${SYSTEM_INSTALL_DIR}" ]] && [ -f "${_SYSTEM_BIN_LINK}" ]; then
-        rm -rvf "${_SYSTEM_BIN_LINK}"
+    if [[ "${ROOT}${INSTALL_DIR}" == "${ROOT}${SYSTEM_INSTALL_DIR}" ]] && [ -f "${ROOT}${_SYSTEM_BIN_LINK}" ]; then
+        rm -rvf "${ROOT}${_SYSTEM_BIN_LINK}"
     fi
 
-    rm -rvf "${INSTALL_DIR}" "${APP_DIR}/remote_tvcmd.desktop"
+    rm -rvf "${ROOT}${INSTALL_DIR}" "${ROOT}${APP_DIR}/remote_tvcmd.desktop"
 fi
 
 exit 0
